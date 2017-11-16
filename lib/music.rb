e5b867f7b78754d70c35d700ed8547ec34e9c6f5
Eric McKinney <mckinney99@gmail.com>
2017-11-16 16:53:59 +0000

    automatically backed up by learn

--- lib/music.rb
+++ lib/music.rb
@@ -26,11 +26,11 @@ class MusicImporter
   end
 
    def call
- 
+
      puts "Welcome to your music library!"
- 
+
      while(true)
- 
+
        puts "To list all of your songs, enter 'list songs'."
        puts "To list all of the artists in your library, enter 'list artists'."
        puts "To list all of the genres in your library, enter 'list genres'."
@@ -39,9 +39,9 @@ class MusicImporter
        puts "To play a song, enter 'play song'."
        puts "To quit, type 'exit'."
        puts "What would you like to do?"
- 
+
        text = gets.chomp()
- 
+
        self.play_song if text == "play song"
        self.list_songs_by_genre if text == "list genre"
        self.list_songs_by_artist if text == "list artist"
@@ -50,49 +50,49 @@ class MusicImporter
        self.list_songs if text == "list songs"
        break if text == "exit"
      end
- 
+
    end
- 
+
    def list_songs
      Song.all.each_with_index {|s, k| puts "#{k+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}" }
    end
- 
+
    def list_artists
      Artist.all.each_with_index {|s, k| puts "#{k+1}. #{s.name}" }
    end
- 
+
    def list_genres
      list = Genre.all.sort_by {|o| o.name}
      list.each_with_index {|s, k| puts "#{k+1}. #{s.name}" }
    end
- 
+
    def list_songs_by_artist
- 
+
      puts "Please enter the name of an artist:"
      name = gets.chomp()
      songs = Song.all.select {|s| s.artist.name == name}
      songs = songs.sort_by {|s| s.name}
      songs.each_with_index {|s, k| puts "#{k+1}. #{s.name} - #{s.genre.name}" }
- 
+
    end
- 
+
    def list_songs_by_genre
- 
+
      puts "Please enter the name of a genre:"
      name = gets.chomp()
      songs = Song.all.select {|s| s.genre.name == name}
      songs = songs.sort_by {|s| s.name}
      songs.each_with_index {|s, k| puts "#{k+1}. #{s.artist.name} - #{s.name}" }
- 
+
    end
- 
+
    def play_song
- 
+
      puts "Which song number would you like to play?"
      number = gets.chomp.to_i
      list = Song.all.sort_by {|s| s.name}
      puts "Playing #{list[number - 1].name} by #{list[number - 1].artist.name}" if number > 0 and number <= list.count
- 
+
    end
- 
+
  end
